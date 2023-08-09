b876d3fd6a8076d7a9da9aa92f62a2dfbd37609c
Devendra patel <128667679+Devendra1patel@users.noreply.github.com>
2023-08-09 13:02:04 +0530

    Add files via upload

---
+++ Project01_css_html/script.js
@@ -0,0 +1,25 @@
+// document.body.childNodes
+// document.body.childNodes[1]
+// const a =Array.from(document.body.childNodes)
+// console.log(a.length)
+//  [33]
+// console.log("i am robbo collector")
+// console.log("First Child is;=",home.firstElementChild)
+// console.log("First Child is;=",home.firstChild)
+// #now we make change's use nevigation :in css
+// const headd = () => {
+//     navbar.style.background="yellow"
+    // navbar.style.color="white"
+    // console.log("ajdofaeuhif")
+// }
+// console.log(headd())
+// headd()
+// headd = () => {
+        // home.tmp.style.background="yellow"
+        // navbar.style.color="white"
+        // console.log("ajdofaeuhif")
+    // }
+
+// #[34] table
+let t = document.table
+console.log(t)
